c7c097f2e7501322d1e02b50460e83b1c8d86901
xyf <1713893320@qq.com>
2019-10-06 16:35:34 +0800

    修改java-web-dev部分内容

--- library/src/com/web/service/BookService.java
+++ library/src/com/web/service/BookService.java
@@ -1,18 +1,16 @@
-package com.web;
+package com.web.service;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import com.web.entity.Book;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@WebServlet(urlPatterns = "/index")
-public class IndexServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+public class BookService {
+    private static List<Book> bookList;
+
+    public static List<Book> init() {
+        bookList = new ArrayList<>(10);
         Book[] books = {
                 new Book("我是猫", "我是猫.jpg", "夏目漱石"),
                 new Book("人间失格", "人间失格.jpg", "太宰治"),
@@ -47,9 +45,8 @@ public class IndexServlet extends HttpServlet {
                 new Book("汪曾祺精选集", "汪曾祺精选集.jpg", "汪曾祺"),
                 new Book("张爱玲给我的信件","张爱玲给我的信件.jpg","夏志清")
         };
-        List<Book> bookList = Arrays.asList(books);
-        req.setAttribute("bookList",bookList);
-        req.getRequestDispatcher("index.jsp").forward(req,resp);
+        bookList = Arrays.asList(books);
+        return bookList;
 
     }
 }
